2df4a2ccbb2e03e1c6accee5dc5a286207b41f58
Sandeep Shukla <sanshukla@deloitte.com>
2020-06-03 18:52:55 +0530

    Adding product summary section to cart.

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -16,6 +16,8 @@ export class CartComponent implements OnInit {
   product: Observable<ProductStore>;
   cartItemSubscription: Subscription;
   itemCount: number = 0;
+  totalPrice: number = 0;
+  totalDiscount: number = 0;
   constructor(private store:Store<fromProductStore.ProductState>) { }
 
   ngOnInit(): void {
@@ -24,6 +26,12 @@ export class CartComponent implements OnInit {
     this.cartItemSubscription = this.product.subscribe((store: ProductStore) => {
       this.itemCount = store.shoppingList.reduce((count: number, product:Product) => {
         return count + product.itemsInCart
+      }, 0);
+      this.totalPrice = store.shoppingList.reduce((price: number, product:Product) => {
+        return price + (product.price.price * product.itemsInCart);
+      }, 0);
+      this.totalDiscount = store.shoppingList.reduce((price: number, product:Product) => {
+        return price + (product.price.specialPrice > 0 ?  (product.price.price - product.price.specialPrice) * product.itemsInCart: 0);
       }, 0);
     })
   }
